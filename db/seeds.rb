0cb9b2c3c7d4f730f8d75c13b25bb106dbfee140
Lucy Silver <lucy.silver2@gmail.com>
2020-03-22 18:02:53 +0000

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+User.destroy_all
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 Ingredient.create(name: "gin")
 Ingredient.create(name: "egg white")
+
+user = User.create!(email: "test1@test.com", password:"123123")
