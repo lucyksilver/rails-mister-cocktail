069eaa6c031a0c3786e8d917a28e1e4bfe4fdb12
Lucy Silver <lucy.silver2@gmail.com>
2020-03-23 11:26:56 +0000

    back end

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,7 @@ class Cocktail < ApplicationRecord
   has_one_attached :photo
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  belongs_to :user
 
   validates :name, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Dose.destroy_all
 Ingredient.destroy_all
 User.destroy_all
 
